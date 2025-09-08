4bd94b2cdfa6ba1b96fdbe4097ab5a3393e3f0c2
GlassHammerMaster <h3nryr4lph@gmail.com>
2025-09-08 08:08:33 -0700

    Added a few more lines idk

--- lecture2.cpp
+++ lecture2.cpp
@@ -4,35 +4,42 @@ int main() {
     bool x;
     int y;
     int z;
-    std::cout << "enter binary value ";
-    std::cin >> x;
+    int w = 0;
+    while (w < 10) {
+        std::cout << w << " ";
+        w++;
+    }
+    std::cout << "\nenter binary value (0 or 1): ";
+    int temp;
+    std::cin >> temp;
+    x = (temp != 0); // convert to bool
     if (x) {
         std::cout << "true" << std::endl;
     } else {
         std::cout << "false" << std::endl;
     }
-    std::cout << "enter a number ";
+    std::cout << "enter a number: ";
     std::cin >> y;
-    std::cout << "enter another number ";
+    std::cout << "enter another number: ";
     std::cin >> z;
-    if (y>z){
+    if (y > z) {
         std::cout << y << " is greater than " << z;
-    } else if (y<z) {
+    } else if (y < z) {
         std::cout << y << " is less than " << z;
     } else {
         std::cout << y << " is equal to " << z;
     }
     std::cout << std::endl;
 
-    switch(y) {
-        case 1:  
+    switch (y) {
+        case 1:
             std::cout << "y is 1";
-            break; 
-        case 2:
-        case 3: 
-            std::cout << "y is 2 or 3"; 
             break;
-        default:  
+        case 2:
+        case 3:
+            std::cout << "y is 2 or 3";
+            break;
+        default:
             std::cout << "y is not 1, 2, or 3";
     }
     return 0;
